57f7c86e4e473391628e2d4a86ac3a6e89ac9711
Snowkai <34677392+Snowkai@users.noreply.github.com>
2024-02-14 01:27:36 +0300

    dev

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using static Unity.Collections.AllocatorManager;
 
 public class Card : MonoBehaviour
